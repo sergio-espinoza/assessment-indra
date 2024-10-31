77c792603d61e66840aff328f8533610be610339
Sergio Junior Espinoza Peralta <48479567.sjep@gmail.com>
2024-10-31 12:59:28 -0500

    feat(asm-005): implement pagination

--- src/app/features/product/product-list/product-list.component.ts
+++ src/app/features/product/product-list/product-list.component.ts
@@ -1,12 +1,12 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { ProductItemComponent } from '../product-item/product-item.component';
-import { debounceTime, distinctUntilChanged, filter, map, of, switchMap, tap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, of, Subscription, switchMap, tap } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { Product, ProductResponse } from '../product.interface';
 import { DEFAULT_LIMIT_SIZE, INITIAL_PAGE } from '../product.constant';
 import { ProductService } from '../services';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators as Vl } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
@@ -15,16 +15,19 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   private _router = inject(Router);
   private _productSvc = inject(ProductService);
 
-  searchControl = new FormControl('');
-
-  productLimit = signal(DEFAULT_LIMIT_SIZE);
-  productPage = signal(INITIAL_PAGE);
+  filterForm = new FormGroup({
+    search: new FormControl('', [Vl.required]),
+    limit: new FormControl(DEFAULT_LIMIT_SIZE),
+    page: new FormControl(INITIAL_PAGE)
+  });
 
   products$ = of<Product[]>([]);
+
+  subscriptions = new Subscription();
 
   constructor() {
   }
@@ -41,9 +44,7 @@ export class ProductListComponent implements OnInit {
   remove(id: string) {
     this._productSvc.removeProduct$(id).pipe(
       switchMap(
-        () => this._productSvc.searchProducts$(
-          this.getParamsToSearch(this.searchControl.value || '')
-        )
+        () => this._productSvc.searchProducts$(this.getParamsToSearch())
       ),
       map(response => response.filter(
         item => !this._productSvc.getRemovedProducts().includes(`${item.id}`)
@@ -52,37 +53,59 @@ export class ProductListComponent implements OnInit {
   }
 
   initSearchHandler() {
-    this.searchControl.valueChanges.pipe(
+    let currentSearchText = '';
+    let currentPage = INITIAL_PAGE;
+
+    const filterSubscription = this.filterForm.valueChanges.pipe(
       debounceTime(500),
-      distinctUntilChanged(),
-      filter(value => (value || '')?.length > 3),
+      filter(value => {
+        if (currentPage !== value.page) return true;
+
+        return currentSearchText !== value.search && (value?.search || '')?.length > 3;
+      }),
+      map(value => {
+        currentSearchText = value.search || '';
+        currentPage = value.page || 0;
+
+        return { ...value, search: currentSearchText, page: currentPage };
+      }),
       switchMap(
-        value => this._productSvc.searchProducts$(this.getParamsToSearch(value || ''))
+        () => this._productSvc.searchProducts$(this.getParamsToSearch())
       ),
       map(response => response.filter(
         item => !this._productSvc.getRemovedProducts().includes(`${item.id}`)
       ))
     ).subscribe(response => this.products$ = of(response));
+
+    this.subscriptions.add(filterSubscription);
+  }
+
+  goToPage(page: number) {
+    this.filterForm.patchValue({ page });
   }
 
   loadProducts() {
-    const params = {
-      limit: `${this.productLimit()}`,
-      skip: `${this.productPage()}`,
-    }
-
-    this.products$ = this._productSvc.getProducts$(params).pipe(
+    this.products$ = this._productSvc.getProducts$(this.getParamsToSearch()).pipe(
       map(response => response.filter(
         item => !this._productSvc.getRemovedProducts().includes(`${item.id}`)
       ))
     );
   }
 
-  getParamsToSearch(searchString: string) {
+  getParamsToSearch() {
+    const { limit, page, search = '' } = this.filterForm.value;
+
+    const searchText = search || '';
+    const skip = `${(page || 0) * (limit || DEFAULT_LIMIT_SIZE)}`;
+
     return {
-      limit: `${this.productLimit()}`,
-      skip: `${this.productPage()}`,
-      q: searchString
+      limit: `${limit}`,
+      skip,
+      ...(searchText.length > 0 ? { q: searchText } : {})
     }
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
